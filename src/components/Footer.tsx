f7889082cbbdf6c37ab5fadea304869f0e1451e7
Saadin Dassum <34032935+saadindassum@users.noreply.github.com>
2026-07-28 19:14:34 -0500

    Added link to Saadin Dassum's LinkedIn on website footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>©{new Date().getFullYear()} George Muir &nbsp;|&nbsp; ALL RIGHTS RESERVED &nbsp;|&nbsp; Florida Sellers of Travel ARC# 17873 &nbsp;|&nbsp; Website by Saadin Dassum</p>
+      <p>©{new Date().getFullYear()} George Muir &nbsp;|&nbsp; ALL RIGHTS RESERVED &nbsp;|&nbsp; Florida Sellers of Travel ARC# 17873 &nbsp;|&nbsp; Website by <a href="https://www.linkedin.com/in/saadin-dassum/">Saadin Dassum</a></p>
     </footer>
   );
 }
